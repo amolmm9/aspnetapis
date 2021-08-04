19302dadd4d3ae167f132a7da82222b86ea82043
Amol Manthalkar <amolmm9@gmail.com>
2021-08-04 19:09:26 +0530

    added swagger

--- apis/src/CatalogApi/Startup.cs
+++ apis/src/CatalogApi/Startup.cs
@@ -26,6 +26,7 @@ namespace CatalogApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,8 +36,17 @@ namespace CatalogApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
 
-            app.UseHttpsRedirection();
+            app.UseSwaggerUI(c =>
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1")
+            ); 
+
+            app.UseHttpsRedirection(); 
 
             app.UseRouting();
 
